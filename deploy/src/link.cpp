6ae99ce70e72b62bfcb371f0e758ea2dbc65cf2f
zhaoxi <535394140@qq.com>
2026-02-24 14:39:49 +0800

    (fix) deploy: Timer 测试修改为 Time

--- deploy/src/link.cpp
+++ deploy/src/link.cpp
@@ -1,12 +1,8 @@
-#include <thread>
+#include <fmt/format.h>
 
 #include <rmvl/core.hpp>
 
-using namespace std::chrono_literals;
-
 int main() {
-    rm::Timer t{};
-    t.reset();
-    std::this_thread::sleep_for(100ms);
-    printf("now: %f\n", t.now());
+    rm::Time t{};
+    fmt::println("now: {}", t.now());
 }
